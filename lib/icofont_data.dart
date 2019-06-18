ba3b94d6b650b05784da6a092c0f8e41dc9e5fe2
Priyo Mukul <priyomukul@gmail.com>
2019-06-18 13:45:05 +0600

    Brands Icon Added

--- lib/icofont_data.dart
+++ lib/icofont_data.dart
@@ -10,3 +10,31 @@ class IcoFontData extends IconData {
           fontPackage: 'icofont_flutter',
         );
 }
+
+class IcoFontDataWebApps extends IcoFontData {
+  const IcoFontDataWebApps(int codePoint) : super(codePoint);
+}
+
+class IcoFontDataBrands extends IcoFontData {
+  const IcoFontDataBrands(int codePoint) : super(codePoint);
+}
+
+class IcoFontDataDirectional extends IcoFontData {
+  const IcoFontDataDirectional(int codePoint) : super(codePoint);
+}
+
+class IcoFontDataSocial extends IcoFontData {
+  const IcoFontDataSocial(int codePoint) : super(codePoint);
+}
+
+class IcoFontDataAnimal extends IcoFontData {
+  const IcoFontDataAnimal(int codePoint) : super(codePoint);
+}
+
+class IcoFontDataCurrency extends IcoFontData {
+  const IcoFontDataCurrency(int codePoint) : super(codePoint);
+}
+
+class IcoFontDataFood extends IcoFontData {
+  const IcoFontDataFood(int codePoint) : super(codePoint);
+}
